7442b087ef37f1afd168e35884cac0311e2329f5
James Turk <james.p.turk@gmail.com>
2015-03-12 01:38:07 -0400

    http for now, fix publish step

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-rm -rf public/ && hugo && s3personal sync -MP public/ s3://jamesturk.net
+rm -rf public/ && hugo && s3cmd -c ~/.s3cmd/personal sync -MP public/ s3://jamesturk.net && s3cmd -c ~/.s3cmd/personal put -P --mime-type=text/css public/css/main.css s3://jamesturk.net/css/main.css
