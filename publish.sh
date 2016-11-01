5399bbbed9bf9c8de18d3dcf6af4e419951d8ac1
James Turk <james.p.turk@gmail.com>
2016-11-01 13:41:58 -0400

    .

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 rm -rf public/ &&
 hugo &&
-s3cmd -c ~/.s3cmd/personal sync -MP public/ s3://jamesturk.net &&
-s3cmd -c ~/.s3cmd/personal put -P --mime-type=text/css public/css/main.css s3://jamesturk.net/css/main.css &&
+s3cmd -c ~/.s3cfg sync -MP public/ s3://jamesturk.net &&
+s3cmd -c ~/.s3cfg put -P --mime-type=text/css public/css/main.css s3://jamesturk.net/css/main.css &&
 ./cf.py
