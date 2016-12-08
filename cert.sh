03881d22efbb440469cd87b4b292a75c4ba21d96
James Turk <james.p.turk@gmail.com>
2016-12-08 00:05:32 -0500

    stop using letsencrypt for this cert

--- cert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# set AWS_ACCESS_KEY_ID & AWS_SECRET_ACCESSPKEY
-../letsencrypt/letsencrypt-auto --agree-tos -a letsencrypt-s3front:auth \
-    --letsencrypt-s3front:auth-s3-bucket jamesturk.net \
-    -i letsencrypt-s3front:installer \
-    --letsencrypt-s3front:installer-cf-distribution-id E22K2SQ1U6924J \
-    -d jamesturk.net -d www.jamesturk.net --debug
