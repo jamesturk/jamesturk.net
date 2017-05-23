9e5f3eeeb4342d0cadf4dcd870bcbea10b60ac18
James Turk <james.p.turk@gmail.com>
2017-05-22 21:59:01 -0700

    update publish to use aws cli

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
+
 AWS_DEFAULT_PROFILE=james
 rm -rf public/ &&
 hugo &&
-s3cmd -c ~/.s3cfg sync -MP public/ s3://jamesturk.net &&
-s3cmd -c ~/.s3cfg put -P --mime-type=text/css public/css/main.css s3://jamesturk.net/css/main.css &&
-./cf.py
+aws s3 sync --acl public-read public/ s3://jamesturk.net &&
+aws cloudfront create-invalidation --distribution-id E22K2SQ1U6924J --path "/*"
