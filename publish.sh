2feadeb72ac7819ff7ba90519448b60f51765f85
James Turk <dev@jamesturk.net>
2019-06-18 17:36:43 -0400

    switch to netlify

--- publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-AWS_DEFAULT_PROFILE=james
-rm -rf public/ &&
-hugo &&
-aws s3 sync --acl public-read public/ s3://jamesturk.net &&
-aws cloudfront create-invalidation --distribution-id E22K2SQ1U6924J --path "/*"
